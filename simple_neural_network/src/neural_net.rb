3d79081978fdc065a1834a8ac9b9d8b1ee04c14c
Alex Pahom <alex.pahomchik@gmail.com>
2020-04-27 23:24:39 +0300

    minor adjustments

--- simple_neural_network/src/neural_net.rb
+++ simple_neural_network/src/neural_net.rb
@@ -7,19 +7,19 @@ require 'pry'
 class NeuralNet
 
   attr_reader :hidden_func, :output_func, :mode, :hidden_nodes, :alpha
-  IMG_AREA = 784
-  CLASSES_NUMBER = 10
+  IMG_AREA = 784 # as the image is 28x28
+  DIGITS_COUNT = 10
 
-  def initialize(opts = {})
+  def initialize(params = {})
     puts_hrow
 
-    @hidden_func = opts[:hidden_func]
-    @output_func = opts[:output_func]
-    @mode = opts[:mode]
-    @hidden_nodes = opts[:hidden_nodes]
+    @hidden_func = params[:hidden_func]
+    @output_func = params[:output_func]
+    @mode = params[:mode]
+    @hidden_nodes = params[:hidden_nodes]
     @error_history = []
     @classification_history = []
-    @alpha = opts[:alpha]
+    @alpha = params[:alpha]
   end
 
   def go
@@ -45,11 +45,20 @@ class NeuralNet
   end
 
   def initialize_new_weights
-    init_factor_1 = 0.01 / ((IMG_AREA + 1) ** (0.5))    # weight scaler for the first weight matrix. It is indirectly proportional to the input size. It has to be, otherwise you will blow up your hidden layer. If you do not put in the deflator. The first couple of itterations will work on large sums in the hidden layer. Large sums are bad in neural networks.
-    init_factor_2 = 0.01 / (hidden_nodes ** (0.5))
-    
-    @w1 = (NMatrix.random([IMG_AREA + 1, hidden_nodes]) - (NMatrix.ones([IMG_AREA + 1, hidden_nodes]) /2)) * init_factor_1 # some basic matrix algebra, create a matrix with
-    @w2 = (NMatrix.random([hidden_nodes + 1, CLASSES_NUMBER])  - (NMatrix.ones([hidden_nodes + 1, CLASSES_NUMBER]) / 2)) * init_factor_2
+    # weight scaler for the first weight matrix. It is indirectly proportional to the input size.
+    # It has to be, otherwise you will blow up your hidden layer. If you do not put in the deflator.
+    # The first couple of iterations will work on large sums in the hidden layer.
+    # Large sums are bad in neural networks.
+    init_factor_1 = 0.01 / Math.sqrt(IMG_AREA + 1)
+    init_factor_2 = 0.01 / Math.sqrt(hidden_nodes)
+
+    # 785x300 in case of defaults
+    input_to_hidden_shape = [IMG_AREA + 1, hidden_nodes]
+    # 301x10 in case of defaults
+    hidden_to_output_shape = [hidden_nodes + 1, DIGITS_COUNT]
+
+    @w1 = (NMatrix.random(input_to_hidden_shape) - NMatrix.new(input_to_hidden_shape, 0.5)) * init_factor_1 # some basic matrix algebra, create a matrix with
+    @w2 = (NMatrix.random(hidden_to_output_shape)  - NMatrix.new(hidden_to_output_shape, 0.5)) * init_factor_2
   end
   
   def load_trained_weights
@@ -58,44 +67,42 @@ class NeuralNet
   end
 
   def create_test_submission
-   puts "Creating submission" 
+   puts 'Creating submission'
    load_trained_weights
    @hidden_nodes = @w1.shape[1] 
 
-    # data_table = DataTable.new(file: '../data/manual_test_set.csv', label_index: :none)
-    data_table = DataTable.new(file: '../data/mnist_digits/test.csv', label_index: :none)
+    data_table = DataTable.new(file: '../data/manual_test_set.csv', label_index: :none)
+    # data_table = DataTable.new(file: '../data/mnist_digits/test.csv', label_index: :none)
 
-    CSV.open("../data/submission.csv", "wb") do |csv|
-      csv << %w(ImageID Label)
+    CSV.open('../data/submission.csv', 'wb') do |csv|
+      csv << %w(ImageID Digit)
 
       data_table.observations.each_with_index do |observation, i|
         print '.' if (i + 1) % 100 == 0
         puts if (i + 1) % 10000 == 0
-        csv << [(i + 1).to_s, forward(observation, eval_or_train: 'eval')]
+        csv << [(i + 1).to_s, forward(observation)]
       end
       puts
     end
   end 
 
-  def forward(observation, opts = {})
-    #convert the features array into a NMatrix matrix and divide every element by 255.
-    #the division scales down the input.The input vector is initialized with size
-    #1 bigger than the IMG_AREA. This is to accommodate the bias term
+  def forward(observation)
+    # convert the features array into a NMatrix matrix and divide every element by 255.
+    # the division scales down the input. The input vector is initialized with size
+    # 1 bigger than the IMG_AREA. This is to accommodate the bias term
     a1 = observation.features.flatten.to_nm([1, IMG_AREA + 1]) / 255.0
 
-    #Set the bias term equal to 1
-    #NMatrix 2 dimensional matrices can be accessed via [row,column]
+    # Set the bias term equal to 1
     a1[0, IMG_AREA] = 1.0
 
-    #pass the product of the input values and the arc weight forward
-    #and sum the product up at each node
-
+    # pass the product of the input values and the weight forward
+    # and sum the product up at each node
     z2 = a1.dot(@w1)
 
-    #apply the activation function to the sum vector element wise
+    # apply the activation function to the sum vector element wise
     a2 = activation_function(z2, hidden_func)
 
-    #resize the hidden layer to add the bias unit
+    # resize the hidden layer to add the bias unit
     a2_with_bias = NMatrix.zeroes([1, hidden_nodes + 1])
     a2_with_bias[0, 0..hidden_nodes] = a2
     a2_with_bias[0, hidden_nodes] = 1.0
@@ -106,56 +113,54 @@ class NeuralNet
     #Softmax activation function in the output layer
     a3 = activation_function(z3, @output_func)
 
-    #if in training mode, pass values of layers to backprop.
-    #otherwise return the prediction the output layer
-    if opts[:eval_or_train] == 'train'
+    # if in training mode, pass values of layers to backprop.
+    # otherwise return the prediction the output layer
+    case mode
+    when 'train'
       backprop(a1, a2_with_bias, z2, z3, a3, observation.label)
-    elsif opts[:eval_or_train] == 'eval'
+    when 'eval'
       return a3.each_with_index.max[1]
     end
   end
   
 def backprop(a1, a2_with_bias, z2, z3, a3, label)
-  #initiates the output vector of zeroes
+  # initiates the output vector of zeroes
   y = NMatrix.zeroes([1, 10])
 
-  #set the label from the data to 1
-  #only 1 element can be 1 at a time as classes
-  #are mutually exclusive
+  # set the label from the data to 1
+  # only 1 element can be 1 at a time as classes
+  # are mutually exclusive
   y[0, label] = 1.0
   
-  #derivative of the loss function. Difference between predicted
-  #values and the true value  
-  
+  # derivative of the loss function. Difference between predicted
+  # values and the true value
   d3 = -(y - a3)  
 
-  #using the derivative d3 is a good enough measure to 
-  #see if the cost is decreasing so we append it to
-  #the error history
+  # using the derivative d3 is a good enough measure to
+  # see if the cost is decreasing so we append it to
+  # the error history
   @error_history << d3.transpose.abs.sum[0]
   
-  #add 1 to the classification history if the prediction
-  #is correct, otherwise zero
+  # add 1 to the classification history if the prediction is correct, otherwise zero
   @classification_history << (a3.each_with_index.max[1] == label ? 1.0 : 0.0)
     
   # derivative, has the same size as the hidden layer. The range [] operator
   # excludes the bias node. No error is passed to the bias node.  
   d2 = @w2.dot(d3.transpose)[0..(@hidden_nodes-1)] * derivative(z2.transpose, @hidden_func)
   
-  #matrix with dimensions equal to @w1's dimensions
-  #each element contains the gradient of the weight
-  #with respect to the cost function. If the weights
-  #are reduced by a small fraction of this value the cost function
-  #will go down     
+  # matrix with dimensions equal to @w1's dimensions
+  # each element contains the gradient of the weight
+  # with respect to the cost function. If the weights
+  # are reduced by a small fraction of this value the cost function
+  # will go down
   grad1 = d2.dot(a1)
   
-  #same for @w2 
+  # same for @w2
   grad2 = d3.transpose.dot(a2_with_bias)
 
   # updating the weigh matrices. The first layer is updated
   # by a factor of 10 less than than the second layer. for numerical
   # stability. Big weight changes -> big weights -> equals big sums -> saturated neurons
-    
   @w1 -= grad1.transpose * alpha * 0.1
   @w2 -= grad2.transpose * alpha
 end
@@ -170,7 +175,7 @@ def train
     # forward pass in the network with a random observation from @dt.sample.
     # eval_or_train is passed as train becuase the forward method has to pass
     # it's results to the backprop method. The backprop method will update the weights
-    forward(@dt.sample, eval_or_train: 'train')
+    forward(@dt.sample)
 
     ave_error_history_1k = running_average(1000, @error_history)
     ave_error_history_5k = running_average(5000, @error_history)
@@ -190,8 +195,8 @@ def train
     if ratio < 1.0 and i > 2000
     # if ratio < 1.0 and i > 60000
       finish_time = Time.now
-      puts File.open('../data/w1.txt', 'w') { |f| f << Marshal.dump(@w1.to_a) }
-      puts File.open('../data/w2.txt', 'w') { |f| f << Marshal.dump(@w2.to_a) }
+      File.open('../data/w1.txt', 'w') { |f| f << Marshal.dump(@w1.to_a) }
+      File.open('../data/w2.txt', 'w') { |f| f << Marshal.dump(@w2.to_a) }
       puts "Total training time was: #{(finish_time - start_time).round(0)} sec"
       break
     end
